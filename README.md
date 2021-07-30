3b7b7562f5bd94d8716fd73f3107623ce7f6fce7
samirmalekdev <samirmalek.dev@gmail.com>
2021-07-31 00:09:14 +0300

    added firebase config file and readme updated

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # amazon-clone-v2.0
 
-
 ### Building with with HTML5 CSS3 JS6 with tailwind CSS for Front-End and Google Firebase for backend Database
+## Backend is written in JS Programming language and for DataBase - Google FireBase is used  , 
+### Replace with the config file with your own SDK file from your google firebase app settings website.
---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyACmDab7dw1wvjgYDLTPAX3xrHMAbYbSbY",
+    authDomain: "clone-v2-3e07f.firebaseapp.com",
+    projectId: "clone-v2-3e07f",
+    storageBucket: "clone-v2-3e07f.appspot.com",
+    messagingSenderId: "145570465329",
+    appId: "1:145570465329:web:c07cea9b8415973408f710"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
