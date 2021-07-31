5259d504384538144ba93eaffcf0f4a5e0f8e82d
samirmalekdev <samirmalek.dev@gmail.com>
2021-07-31 16:46:20 +0300

    added firebase file

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-<script>
-  var firebaseConfig = {
-    apiKey: "AIzaSyACmDab7dw1wvjgYDLTPAX3xrHMAbYbSbY",
-    authDomain: "clone-v2-3e07f.firebaseapp.com",
-    projectId: "clone-v2-3e07f",
-    storageBucket: "clone-v2-3e07f.appspot.com",
-    messagingSenderId: "145570465329",
-    appId: "1:145570465329:web:c07cea9b8415973408f710"
-  };
-  firebase.initializeApp(firebaseConfig);
-</script>
+var firebaseConfig = {
+  apiKey: "AIzaSyACmDab7dw1wvjgYDLTPAX3xrHMAbYbSbY",
+  authDomain: "clone-v2-3e07f.firebaseapp.com",
+  projectId: "clone-v2-3e07f",
+  storageBucket: "clone-v2-3e07f.appspot.com",
+  messagingSenderId: "145570465329",
+  appId: "1:145570465329:web:c07cea9b8415973408f710",
+  measurementId: "G-LYV7E3VYKR",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
